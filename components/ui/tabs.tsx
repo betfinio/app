ecf050401944eb37764ca6a8aa5acfbd0db3afe5
bfmonster <bf.monster@proton.me>
2024-07-16 19:40:45 +0200

    added table componenet with default table

--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -12,7 +12,7 @@ const TabsList = React.forwardRef<
 	<TabsPrimitive.List
 		ref={ref}
 		className={cn(
-			"inline-flex bg-primary h-10 items-center justify-start p-2",
+			"inline-flex bg-primary h-10 overflow-x-scroll w-full flex-nowrap items-center justify-start gap-2",
 			className
 		)}
 		{...props}
@@ -42,7 +42,7 @@ const TabsContent = React.forwardRef<
 	<TabsPrimitive.Content
 		ref={ref}
 		className={cn(
-			"mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
+			"mt-2 w-full ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
 			className
 		)}
 		{...props}
