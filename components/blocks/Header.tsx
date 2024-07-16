55264531393cc284e703259f5aa6cb64b2698714
bfmonster <bf.monster@proton.me>
2024-07-16 14:29:15 +0200

    fixed layout

--- components/blocks/Header.tsx
+++ components/blocks/Header.tsx
@@ -5,14 +5,14 @@ import {Menu} from "@betfinio/ui/dist/icons";
 import {Link} from "@tanstack/react-router";
 
 const Header: FC<PropsWithChildren<{ sidebar: ReactNode }>> = ({children, sidebar}) => {
-	return <header className={'h-[70px] flex flex-row items-center justify-between lg:justify-end px-4 border-b border-gray-800'}>
+	return <header className={'h-[70px] flex flex-row items-center justify-between lg:!justify-end px-4 border-b border-gray-800'}>
 		<Link to={'/'}>
-			<Logo className={'md:hidden'} size={'small'}/>
-			<Logo className={'hidden md:block lg:hidden h-[30px]'} size={'big'}/>
+			<Logo className={'md:!hidden'} size={'small'}/>
+			<Logo className={'hidden md:!block lg:!hidden h-[30px]'} size={'big'}/>
 		</Link>
 		{children}
 		<Sheet>
-			<SheetTrigger className={'lg:hidden'}><Menu className={'text-white'}/></SheetTrigger>
+			<SheetTrigger className={'lg:!hidden'}><Menu className={'text-white'}/></SheetTrigger>
 			<SheetContent className={'dark text-white'}>
 				<SheetTitle/>
 				<SheetDescription/>
