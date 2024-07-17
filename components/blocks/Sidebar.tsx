322baeca96074237cbc58a95b98f1b2230499833
bfmonster <bf.monster@proton.me>
2024-07-17 14:29:05 +0200

    fixed link fetching for conservative stkaing and app

--- components/blocks/Sidebar.tsx
+++ components/blocks/Sidebar.tsx
@@ -2,13 +2,13 @@ import {FC, PropsWithChildren, ReactNode} from "react";
 import Logo from "@/components/ui/logo.tsx";
 import NavGroup from "@/components/ui/NavGroup.tsx";
 import {NavItemProps} from "@/components/ui/NavItem.tsx";
-import {Link} from "@tanstack/react-router";
+import {getAppUrl} from "@/src/config/links.tsx";
 
 const Sidebar: FC<PropsWithChildren<{ links: NavItemProps[][] }>> = ({children, links}) => {
 	return <div className={'flex flex-col items-start justify-start px-5 min-h-[100vh] SIDEBAR'}>
-		<Link to={'/'} className={'h-[70px] flex items-center'}>
+		<a href={getAppUrl()} className={'h-[70px] flex items-center'}>
 			<Logo size={'big'}/>
-		</Link>
+		</a>
 		{children}
 		{links.map((items, index) => <NavGroup key={index} links={items}/>)}
 	</div>
