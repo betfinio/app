35d96c8ae6a0357a3e64f34b44d9c607f1cb7b7d
bfmonster <bf.monster@proton.me>
2024-07-29 16:17:56 +0200

    added fetching by timeframe

--- lib/api/dynamic.ts
+++ lib/api/dynamic.ts
@@ -1,25 +1,25 @@
 import {SupabaseClient} from "@supabase/supabase-js";
 import {valueToNumber} from "@betfinio/abi/dist";
-import {Stat} from "@/lib/types/staking";
+import {Stat, Timeframe} from "@/lib/types/staking";
 
-export const fetchTotalStakedStat = async (supabase: SupabaseClient): Promise<Stat[]> => {
+export const fetchTotalStakedStat = async (timeframe: Timeframe, supabase: SupabaseClient): Promise<Stat[]> => {
 	const data = await supabase
-		.from("staking_statistics_view_day")
+		.from(`staking_statistics_view_${timeframe}`)
 		.select("time, staked::text")
 		.eq('staking', import.meta.env.PUBLIC_DYNAMIC_STAKING_ADDRESS.toLowerCase())
 	return data.data!.map(e => ({time: new Date(e.time).getTime() / 1000, value: valueToNumber(BigInt(e.staked))}));
 }
 
-export const fetchTotalStakersStat = async (supabase: SupabaseClient): Promise<Stat[]> => {
+export const fetchTotalStakersStat = async (timeframe: Timeframe, supabase: SupabaseClient): Promise<Stat[]> => {
 	const data = await supabase
-		.from("staking_statistics_view_day")
+		.from(`staking_statistics_view_${timeframe}`)
 		.select("time, stakers::text")
 		.eq('staking', import.meta.env.PUBLIC_DYNAMIC_STAKING_ADDRESS.toLowerCase())
 	return data.data!.map(e => ({time: new Date(e.time).getTime() / 1000, value: Number(e.stakers)}));
 }
-export const fetchTotalProfitStat = async (supabase: SupabaseClient): Promise<Stat[]> => {
+export const fetchTotalProfitStat = async (timeframe: Timeframe, supabase: SupabaseClient): Promise<Stat[]> => {
 	const data = await supabase
-		.from("staking_statistics_view_day")
+		.from(`staking_statistics_view_${timeframe}`)
 		.select("time, revenues::text")
 		.eq('staking', import.meta.env.PUBLIC_DYNAMIC_STAKING_ADDRESS.toLowerCase())
 	return data.data!.map(e => ({time: new Date(e.time).getTime() / 1000, value: valueToNumber(BigInt(e.revenues))}));
