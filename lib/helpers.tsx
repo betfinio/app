92442f1075159c64be109253689cf844f5f1609e
bfmonster <bf.monster@proton.me>
2024-07-31 13:26:07 +0200

    added helpers and updated packages

---
+++ lib/helpers.tsx
@@ -0,0 +1,8 @@
+import {SquareArrowOutUpRight} from "lucide-react";
+
+export function getTransactionLink(hash: string, label?: string) {
+	return <a href={import.meta.env.PUBLIC_ETHSCAN + '/tx/' + hash} target={'_blank'} className={'flex flex-row items-center justify-between gap-1 pr-10'}>
+		{label}
+		<SquareArrowOutUpRight className={'w-4 h-4'}/>
+	</a>
+}
