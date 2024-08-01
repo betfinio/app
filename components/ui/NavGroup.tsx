e84976d57adca6d1dae1f004ed7281804bc3f927
bfmonster <bf.monster@proton.me>
2024-08-01 11:37:11 +0200

    fixed soon badge on sidebar

--- components/ui/NavGroup.tsx
+++ components/ui/NavGroup.tsx
@@ -6,7 +6,7 @@ const NavGroup: FC<PropsWithChildren<{ links: any[], minimized?: boolean }>> = (
 	const {t} = useTranslation()
 	return <>
 		<div className={'border w-full border-primaryLighter my-4'}></div>
-		<div className={'text-gray-200 flex flex-col gap-4 pt-2'}>
+		<div className={'text-gray-200 flex flex-col gap-4 pt-2 w-full'}>
 			{links.map((item, index) => <NavItem key={index} minimized={minimized} {...item} label={t('shared.sidebar.' + item.label)} active={location.href === item.href}/>)}
 		</div>
 	</>
