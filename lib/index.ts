1d0f16c57960113996834ec162827af4b9734d80
bfmonster <bf.monster@proton.me>
2024-08-07 12:13:30 +0200

    added relative links

--- lib/index.ts
+++ lib/index.ts
@@ -1,15 +1,18 @@
 export const getStakingUrl = (pathname: string = '/') => {
 	if (!pathname.startsWith('/')) pathname = '/' + pathname;
+	if (window.location.origin === import.meta.env.PUBLIC_STAKING_URL) return pathname
 	return import.meta.env.PUBLIC_STAKING_URL + pathname;
 }
 
 export const getAffiliateUrl = (pathname: string = '/') => {
 	if (!pathname.startsWith('/')) pathname = '/' + pathname;
+	if (window.location.origin === import.meta.env.PUBLIC_AFFILIATE_URL) return pathname
 	return import.meta.env.PUBLIC_AFFILIATE_URL + pathname;
 }
 
 export const getGamesUrl = (pathname: string = '/') => {
 	if (!pathname.startsWith('/')) pathname = '/' + pathname;
+	if (window.location.origin === import.meta.env.PUBLIC_GAMES_URL) return pathname
 	return import.meta.env.PUBLIC_GAMES_URL + pathname;
 }
 
