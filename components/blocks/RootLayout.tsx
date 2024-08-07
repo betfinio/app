c0d2327ff48eee0f28fa4d02408858ca58f57def
bfmonster <bf.monster@proton.me>
2024-08-07 17:57:37 +0200

    added footer

--- components/blocks/RootLayout.tsx
+++ components/blocks/RootLayout.tsx
@@ -10,18 +10,20 @@ interface RootLayoutProps {
 }
 
 const RootLayout: FC<PropsWithChildren<RootLayoutProps>> = ({children, footer, header, id, sidebar}) => {
-	return <main className={'w-full min-h-[100vh] max-w-[1440px] mx-auto bg-primary text-white flex flex-row flex-nowrap'}>
-		<section className={'hidden lg:block lg:max-w-[250px] min-h-[100vh]'}>
-			{sidebar}
-		</section>
-		<div className={cx('flex flex-col flex-grow', id)}>
-			{header}
-			{children}
+	return <main className={'w-full min-h-[100vh] max-w-[1440px] mx-auto bg-primary text-white flex flex-col'}>
+		<div className={'flex flex-nowrap'}>
+			<section className={'hidden lg:block lg:max-w-[250px] min-h-[100vh]'}>
+				{sidebar}
+			</section>
+			<div className={cx('flex flex-col flex-grow', id)}>
+				{header}
+				{children}
+			</div>
 		</div>
 		<Toaster/>
-		{/*<div className={'w-full'}>*/}
-		{/*	{footer}*/}
-		{/*</div>*/}
+		<div className={'w-full '}>
+			{footer}
+		</div>
 	</main>
 }
 
