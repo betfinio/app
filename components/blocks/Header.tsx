eb2d522cc19a37febe2a17d3cb2091ef3ece429c
bfmonster <bf.monster@proton.me>
2024-08-09 11:55:54 +0200

    fixed scroll on mobile sidebar

--- components/blocks/Header.tsx
+++ components/blocks/Header.tsx
@@ -4,16 +4,17 @@ import {Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTri
 import {Menu} from "@betfinio/ui/dist/icons";
 import {Link} from "@tanstack/react-router";
 import MemberProfile from "@/components/blocks/MemberProfile.tsx";
+import {ScrollArea} from "@/components/ui/scroll-area.tsx";
 
 const Header: FC<PropsWithChildren<{ sidebar: ReactNode }>> = ({children, sidebar}) => {
-	return <header style={{minHeight: '70px'}} className={' flex flex-row items-center justify-between lg:justify-end px-4 border-b border-gray-800'}>
+	return <header style={{minHeight: '70px'}} className={'flex flex-row items-center justify-between lg:justify-end px-4 border-b border-gray-800'}>
 		<Link to={'/'}>
 			<Logo className={'lg:hidden'} size={'small'}/>
 		</Link>
 		{children}
 		<Sheet>
 			<SheetTrigger className={'lg:hidden'}><Menu className={'text-white'}/></SheetTrigger>
-			<SheetContent className={'dark text-white'}>
+			<SheetContent className={'dark text-white !max-w-[250px] pb-20 overflow-y-auto overflow-x-hidden'}>
 				<SheetTitle/>
 				<SheetDescription/>
 				{sidebar}
