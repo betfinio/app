d7d9e7e34e5ff4e6cd46c55fb68d99fbb48be8e6
bfmonster <bf.monster@proton.me>
2024-08-14 11:30:11 +0200

    fixed dialog

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	darkMode: ['class'],
-	important: 'body',
 	content: ['./components/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
 	theme: {
 		container: {
