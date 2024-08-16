cda2a3a386fd262bbf3802c5518702c87559003d
bfmonster <bf.monster@proton.me>
2024-08-16 11:10:34 +0200

    different fetch for luro online

--- lib/api/luro.ts
+++ lib/api/luro.ts
@@ -1,27 +1,23 @@
 import type { Options } from '@/lib/types';
-import { getBlockByTimestamp } from '@/lib/utils.ts';
-import supabase from '@/src/config/supabase.ts';
-import { BetsMemoryContract } from '@betfinio/abi';
+import { LuckyRoundContract } from '@betfinio/abi';
 import { readContract } from '@wagmi/core';
 import type { Address } from 'viem';
-const BETS_MEMORY_ADDRESS: Address = import.meta.env.PUBLIC_BETS_MEMORY_ADDRESS;
 const LURO_ADDRESS: Address = import.meta.env.PUBLIC_LUCKY_ROUND_ADDRESS;
 
 export async function fetchLuroOnline(options: Options): Promise<number> {
 	if (!options.config || !options.supabase) return 0;
-	const beforeBlock = await getBlockByTimestamp(Math.floor(Date.now() / 1000) - 60 * 60 * 24, supabase);
-	const betsCount = (await readContract(options.config, {
-		abi: BetsMemoryContract.abi,
-		address: BETS_MEMORY_ADDRESS,
-		functionName: 'getGamesBetsCount',
-		args: [LURO_ADDRESS],
-	})) as bigint;
-	const beforeBetsCount = (await readContract(options.config, {
-		abi: BetsMemoryContract.abi,
-		address: BETS_MEMORY_ADDRESS,
-		functionName: 'getGamesBetsCount',
-		args: [LURO_ADDRESS],
-		blockNumber: beforeBlock,
-	})) as bigint;
-	return Number(betsCount) - Number(beforeBetsCount);
+	const round = await readContract(options.config, {
+		abi: LuckyRoundContract.abi,
+		address: LURO_ADDRESS,
+		functionName: 'getCurrentRound',
+	});
+	console.log(round);
+	return Number(
+		await readContract(options.config, {
+			abi: LuckyRoundContract.abi,
+			address: LURO_ADDRESS,
+			functionName: 'getBetsCount',
+			args: [round],
+		}),
+	);
 }
