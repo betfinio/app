883b61a86bc89643bac07cf0716ba55c89cbe722
bfmonster <bf.monster@proton.me>
2024-08-20 20:15:05 +0200

    new conf?

--- src/bootstrap.tsx
+++ src/bootstrap.tsx
@@ -16,5 +16,5 @@ declare module '@tanstack/react-router' {
 const rootElement = document.getElementById('root');
 if (rootElement && !rootElement.innerHTML) {
 	const root = ReactDOM.createRoot(rootElement);
-	root.render(<RouterProvider router={router} />);
+	root.render(<RouterProvider router={router} basepath={'/app'} />);
 }
