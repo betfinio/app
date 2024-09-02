4efdd5f41be128fd714eb9b765561da56b779637
bfmonster <bf.monster@proton.me>
2024-09-02 15:25:02 +0200

    added mint paass function

--- lib/api/pass.ts
+++ lib/api/pass.ts
@@ -1,7 +1,8 @@
 import type { Options } from '@/lib/types';
 import { PassContract } from '@betfinio/abi';
-import type { Address } from 'viem';
+import type { Address, WriteContractReturnType } from 'viem';
 import { readContract } from 'viem/actions';
+import { writeContract } from '@wagmi/core';
 
 export const isMember = async (address: Address | undefined, options: Options): Promise<boolean> => {
 	if (!options.config || !address) return false;
@@ -13,4 +14,14 @@ export const isMember = async (address: Address | undefined, options: Options): 
 			args: [address],
 		})) as bigint) > 0n
 	);
+};
+
+export const mint = async (address: Address, inviter: Address, parent: Address, options: Options): Promise<WriteContractReturnType> => {
+	// biome-ignore lint/style/noNonNullAssertion: <explanation>
+	return await writeContract(options.config!, {
+		address: import.meta.env.PUBLIC_PASS_ADDRESS,
+		abi: PassContract.abi,
+		functionName: 'mint',
+		args: [address, inviter, parent],
+	});
 };
--- lib/query/pass.ts
+++ lib/query/pass.ts
@@ -1,11 +1,67 @@
-import { isMember } from '@/lib/api/pass.ts';
-import config from '@/src/config/wagmi';
-import { useQuery } from '@tanstack/react-query';
-import type { Address } from 'viem';
+import { isMember, mint } from '@/lib/api/pass.ts';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import type { Address, WriteContractReturnType } from 'viem';
+import { useConfig } from 'wagmi';
+import { toast } from '@/components/ui/use-toast.ts';
+import { waitForTransactionReceipt } from 'viem/actions';
+import { getTransactionLink } from '@/lib/helpers.tsx';
+import { useTranslation } from 'react-i18next';
 
 export const useIsMember = (address: Address | undefined) => {
+	const config = useConfig();
 	return useQuery<boolean>({
 		queryKey: ['isMember', address],
 		queryFn: () => isMember(address, { config }),
+	});
+};
+
+export const useMint = () => {
+	const config = useConfig();
+	const { t } = useTranslation('', { keyPrefix: 'shared.errors' });
+	return useMutation<WriteContractReturnType, unknown, { address: Address; inviter: Address; parent: Address }>({
+		mutationKey: ['mint'],
+		mutationFn: (params) => mint(params.address, params.inviter, params.parent, { config }),
+		onError: (e) => {
+			// @ts-ignore
+			if (e?.cause?.reason) {
+				toast({
+					title: 'Failed to mint passes',
+					// @ts-ignore
+					description: t(e.cause.reason),
+					variant: 'destructive',
+				});
+			} else {
+				toast({
+					title: t('unknown'),
+					variant: 'destructive',
+				});
+			}
+		},
+		onSuccess: async (data) => {
+			if (data !== undefined) {
+				const { id, update } = toast({
+					title: 'Minting a pass',
+					description: 'Transaction submitted',
+					variant: 'loading',
+					duration: 60 * 1000,
+				});
+				await waitForTransactionReceipt(config.getClient(), {
+					hash: data,
+				});
+				update({
+					title: 'Pass was minted',
+					description: 'Transaction confirmed',
+					variant: 'default',
+					duration: 5 * 1000,
+					id: id,
+					action: getTransactionLink(data),
+				});
+			} else {
+				toast({
+					title: t('unknown'),
+					variant: 'destructive',
+				});
+			}
+		},
 	});
 };
