3b709c77eabd5ada8b3713ae6b03aac7a750b298
bfmonster <bf.monster@proton.me>
2024-09-05 21:58:56 +0200

    lint

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,9 +1,9 @@
 import * as i18 from 'i18next';
 import type { i18n } from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import czJSON from './translations/cz.json';
 import enJSON from './translations/en.json';
 import ruJSON from './translations/ru.json';
-import czJSON from './translations/cz.json';
 
 const resources = {
 	en: {
