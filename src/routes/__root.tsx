578510029c63b871ed4fdc10e278d3b45693386e
bfmonster <bf.monster@proton.me>
2024-09-05 21:52:29 +0200

    changing languafe

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -6,13 +6,18 @@ import instance from '@/src/i18n.ts';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { Outlet, type RootRoute, createRootRoute } from '@tanstack/react-router';
 import type { i18n } from 'i18next';
-import type { FC } from 'react';
+import {type FC, useEffect} from 'react';
 import { I18nextProvider } from 'react-i18next';
 import { WagmiProvider } from 'wagmi';
 import queryClient from '../config/query.ts';
 import config, { archiveClient } from '../config/wagmi.ts';
 
 export const Root: FC<{ instance: i18n; id: string }> = ({ instance, id }) => {
+	useEffect(() => {
+		if(window.location.hostname.includes('cz')) {
+			instance.changeLanguage('cz');
+		}
+	}, []);
 	return (
 		<WagmiProvider config={config}>
 			<QueryClientProvider client={queryClient}>
