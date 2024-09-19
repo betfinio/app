8cfa24dcd1e0de098f8cb507666447fb76866d7a
Plane <bf.plane@proton.me>
2024-09-19 18:24:10 +0200

    start to extract texts to translation

--- components/blocks/MemberProfile.tsx
+++ components/blocks/MemberProfile.tsx
@@ -18,6 +18,7 @@ import { motion } from 'framer-motion';
 import { AlertCircle, ArrowLeftCircleIcon, ArrowRightCircleIcon, CheckIcon, Copy, Layers3, LinkIcon, Loader, Medal, PencilIcon, X } from 'lucide-react';
 import { DateTime } from 'luxon';
 import { type ChangeEvent, type FC, type PropsWithChildren, useEffect, useMemo, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { useAccount } from 'wagmi';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '../ui/tooltip';
 
@@ -36,6 +37,8 @@ const FadeInDiv = ({ key, children }: PropsWithChildren<{ key: number }>) => {
 };
 
 const MemberProfile = () => {
+	const { t } = useTranslation('', { keyPrefix: 'shared.profileModal' });
+
 	const { data, close } = useOpenProfile();
 	const { address: me } = useAccount();
 	const { data: regDate = 0, ...regDateOther } = useRegistrationDate(data.address || ZeroAddress);
@@ -65,7 +68,7 @@ const MemberProfile = () => {
 
 	const handleCopyAddress = async () => {
 		toast({
-			title: 'Copied to clipboard!',
+			title: t('copied'),
 			variant: 'default',
 		});
 		await navigator.clipboard.writeText(address);
@@ -86,7 +89,7 @@ const MemberProfile = () => {
 
 	const handleCopyLink = async (text: string) => {
 		toast({
-			title: 'Copied to clipboard!',
+			title: t('copied'),
 			variant: 'default',
 		});
 		await navigator.clipboard.writeText(text);
@@ -111,7 +114,7 @@ const MemberProfile = () => {
 									/>
 								</DialogClose>
 
-								<div className={'flex items-center justify-center text-lg font-semibold'}>Member profile</div>
+								<div className={'flex items-center justify-center text-lg font-semibold'}>{t('title')}</div>
 								<div className={'grid lg:grid-cols-6 grid-cols-1 gap-0 lg:gap-4 my-4'}>
 									<motion.div className={'col-span-1 py-4'}>
 										<motion.img
@@ -127,7 +130,7 @@ const MemberProfile = () => {
 									<div className={'col-span-3 flex flex-col gap-3'}>
 										<div className={''}>
 											<label className={'flex flex-col px-2 relative text-sm'}>
-												<div className={'text-gray-400 font-medium'}>Member wallet</div>
+												<div className={'text-gray-400 font-medium'}>{t('wallet')}</div>
 												<div className={'p-2 px-2 pr-8 rounded-lg border bg-primaryLight text-sm border-purple-box'}>{truncateEthAddress(address, 15)}</div>
 												<div className={'absolute bottom-2.5 right-4 w-5 h-5'}>
 													{addressCopied ? (
@@ -144,18 +147,18 @@ const MemberProfile = () => {
 										</div>
 										<div className={''}>
 											<UsernameEdit
-												label={'Username'}
+												label={t('username')}
 												onSave={handleSaveUsername}
 												allowEdit={me.toLowerCase() === address.toLowerCase()}
 												initialValue={username || ''}
 											/>
 										</div>
 										<div className={cx(me.toLowerCase() === address.toLowerCase() && 'hidden')}>
-											<UsernameEdit label={'Custom username'} onSave={handleSaveCustomUsername} allowEdit={true} initialValue={customUsername} />
+											<UsernameEdit label={t('customUsername')} onSave={handleSaveCustomUsername} allowEdit={true} initialValue={customUsername} />
 										</div>
 										<div className={'text-sm'}>
 											<label className={'flex flex-col px-2 py-1 relative'}>
-												<span className={'text-gray-400 font-medium'}>Registration date</span>
+												<span className={'text-gray-400 font-medium'}>{t('registration')}</span>
 												<input
 													type="text"
 													value={regDate > 0 ? DateTime.fromMillis(regDate).toFormat('DD T') : 'Unknown'}
@@ -169,7 +172,7 @@ const MemberProfile = () => {
 										<div className={'border border-gray-800 bg-primaryLighter rounded-3xl p-6 flex flex-row gap-4'}>
 											<People className={'w-12 h-12 text-yellow-400'} />
 											<div className={'flex flex-col justify-center'}>
-												<div className={'text-sm'}>Inviter</div>
+												<div className={'text-sm'}>{t('inviter')}</div>
 												{member?.inviter ? (
 													<>
 														<div className={'font-medium'}>{truncateEthAddress(member.inviter)}</div>
@@ -184,9 +187,9 @@ const MemberProfile = () => {
 										<div className={'border border-gray-800 bg-primaryLighter rounded-3xl p-6 flex flex-row gap-4'}>
 											<Medal className={'w-12 h-12 text-yellow-400'} />
 											<div className={'flex flex-col justify-center'}>
-												<div className={'text-sm'}>Network size</div>
+												<div className={'text-sm'}>{t('network')}</div>
 												<div className={'font-medium'}>
-													{member.count} direct / {Number(member.countLeft + member.countRight)} total
+													{member.count} {t('direct')} / {Number(member.countLeft + member.countRight)} {t('total')}
 												</div>
 											</div>
 										</div>
@@ -194,12 +197,12 @@ const MemberProfile = () => {
 								</div>
 								<div className={'grid grid-cols-1 lg:grid-cols-3 gap-0 lg:gap-4'}>
 									<div className={'col-span-1'}>
-										<div className={'text-xl font-semibold py-4 px-2'}>Member's volume</div>
+										<div className={'text-xl font-semibold py-4 px-2'}>{t('volume.title')}</div>
 										<div className={'flex flex-col gap-4 '}>
 											<div className={'border border-gray-800 bg-primaryLighter rounded-3xl p-6 flex flex-row gap-4'}>
 												<Layers3 className={'w-12 h-12 text-yellow-400'} />
 												<div className={'flex flex-col justify-center'}>
-													<div className={'text-sm'}>Staking volume</div>
+													<div className={'text-sm'}>{t('volume.staking')}</div>
 													<div className={'font-medium'}>
 														<BetValue value={member.volume} withIcon />
 													</div>
@@ -208,7 +211,7 @@ const MemberProfile = () => {
 											<div className={'border border-gray-800 bg-primaryLighter rounded-3xl p-6 flex flex-row gap-4'}>
 												<Blackjack className={'w-12 h-12 text-yellow-400'} />
 												<div className={'flex flex-col justify-center'}>
-													<div className={'text-sm'}>Betting volume</div>
+													<div className={'text-sm'}>{t('volume.betting')}</div>
 													<div className={'font-medium'}>
 														<BetValue value={member.bets} withIcon />
 													</div>
@@ -217,7 +220,7 @@ const MemberProfile = () => {
 										</div>
 									</div>
 									<div className={'col-span-2'}>
-										<div className={'text-xl font-semibold py-4 px-2'}>Member's network volume</div>
+										<div className={'text-xl font-semibold py-4 px-2'}>{t('networkTable.title')}</div>
 										<div className={'border border-gray-800 bg-primaryLighter rounded-3xl px-4 py-4 flex flex-row gap-4 relative'}>
 											<div
 												className={
@@ -226,49 +229,49 @@ const MemberProfile = () => {
 											>
 												<div className={'text-yellow-400 font-medium flex flex-row min-w-[200px] items-center justify-center gap-2'}>
 													<BetValue precision={2} value={totalVolume} withIcon />
-													<span className={'text-white'}> matching volume</span>
+													<span className={'text-white'}> {t('networkTable.matching')}</span>
 												</div>
 											</div>
 											<div className={'overflow-x-hidden w-full'}>
 												<table className={'w-full text-left overflow-x-hidden'}>
 													<thead className={'whitespace-nowrap'}>
 														<tr className={'text-gray-400 border-b border-gray-600'}>
-															<th className={'w-1/3 pb-4 hidden md:block'}>Product</th>
-															<th className={'w-1/3 pb-4 pl-2'}>Direct volume</th>
-															<th className={'w-1/3 pb-4 pl-2'}>Binary volume</th>
+															<th className={'w-1/3 pb-4 hidden md:block'}>{t('networkTable.product')}</th>
+															<th className={'w-1/3 pb-4 pl-2'}>{t('networkTable.direct')}</th>
+															<th className={'w-1/3 pb-4 pl-2'}>{t('networkTable.binary')}</th>
 														</tr>
 													</thead>
 													<tbody className={'whitespace-nowrap'}>
 														<tr className={'border-b border-gray-600'}>
 															<td className={'hidden md:table-cell'}>
 																<div className={'flex flex-row items-center gap-2'}>
-																	<Layers3 className={'w-4 h-4 text-yellow-400'} /> Staking
+																	<Layers3 className={'w-4 h-4 text-yellow-400'} /> {t('networkTable.staking')}
 																</div>
 															</td>
 															<td className={'py-2 pl-2'}>
 																<div className={'flex flex-col'}>
 																	<BetValue value={volume[0]} withIcon />
-																	<span className={'text-sm'}>total staking</span>
+																	<span className={'text-sm'}>{t('networkTable.totalStaking')}</span>
 																</div>
 															</td>
 															<td className={'py-2 pl-2'}>
 																<div className={'flex flex-col'}>
 																	<BetValue value={member.volumeLeft + member.volumeRight} withIcon />
-																	<span className={'text-sm'}>total staking</span>
+																	<span className={'text-sm'}>{t('networkTable.totalStaking')}g</span>
 																</div>
 															</td>
 														</tr>
 														<tr>
 															<td className={'hidden md:table-cell'}>
 																<div className={'flex flex-row items-center gap-2'}>
-																	<Blackjack className={'w-4 h-4 text-yellow-400'} /> Betting
+																	<Blackjack className={'w-4 h-4 text-yellow-400'} /> {t('networkTable.betting')}
 																</div>
 															</td>
 															<td className={'py-2 pl-2'}>
 																<div className={'flex flex-col'}>
 																	<BetValue value={volume[1] / 100n} withIcon />
 																	<div className={'text-sm flex gap-1'}>
-																		total bets
+																		{t('networkTable.totalBets')}
 																		<Tooltip>
 																			<TooltipTrigger>
 																				<AlertCircle className={'text-yellow-400'} width={18} />
@@ -290,7 +293,7 @@ const MemberProfile = () => {
 																<div className={'flex flex-col'}>
 																	<BetValue value={member.betsLeft / 100n + member.betsRight / 100n} withIcon />
 																	<div className={'text-sm flex gap-1'}>
-																		total bets
+																		{t('networkTable.totalBets')}
 																		<Tooltip>
 																			<TooltipTrigger>
 																				<AlertCircle className={'text-yellow-400'} width={18} />
@@ -319,7 +322,7 @@ const MemberProfile = () => {
 								<Hero className={'w-full'} />
 							</div>
 							<div className={'bg-yellow-400 rounded-b-3xl p-6 flex flex-col items-center gap-2 w-full'}>
-								<div className={'text-black font-semibold'}>Invite link</div>
+								<div className={'text-black font-semibold'}>{t('link')}</div>
 								<div
 									className={
 										'bg-primaryLight rounded-xl p-2 text-xs line-clamp-1 overflow-ellipsis max-w-[90%]  px-4 text-white flex flex-row gap-2 items-center cursor-pointer break-all'
