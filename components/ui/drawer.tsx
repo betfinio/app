52e3f2b8f94fdca98ea138e9a8a9e932511dcd50
Monster <bf.monster@proton.me>
2024-10-04 10:46:58 +0200

    fixed things

--- components/ui/drawer.tsx
+++ components/ui/drawer.tsx
@@ -28,7 +28,7 @@ const DrawerContent = React.forwardRef<React.ElementRef<typeof DrawerPrimitive.C
 			<DrawerOverlay />
 			<DrawerPrimitive.Content
 				ref={ref}
-				className={cn('fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-[10px] border border-gray-900 ', className)}
+				className={cn('fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-[10px] border border-gray-900 bg-card', className)}
 				{...props}
 			>
 				{hasLine && <div className="mx-auto mt-4 h-2 w-[100px] rounded-full bg-gray-500" />}
