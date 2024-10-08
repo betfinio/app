3420106e9c778b0d3340da2997a674862e31b419
Monster <bf.monster@proton.me>
2024-10-08 19:04:57 +0200

    banner only on testnet

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -29,16 +29,18 @@ export const Root: FC<{ instance: i18n; id: string }> = ({ instance, id }) => {
 				<SupabaseProvider client={client} archiveClient={archiveClient}>
 					<I18nextProvider i18n={instance}>
 						<AllowanceProvider>
-							<div className={'w-full bg-red-roulette text-white'}>
-								<motion.div
-									initial={{ x: '-300px' }}
-									animate={{ x: '100%' }}
-									transition={{ duration: 10, ease: 'linear', repeat: Number.POSITIVE_INFINITY }}
-									className={''}
-								>
-									This is a test version of BETFIN
-								</motion.div>
-							</div>
+							{!isProd && (
+								<div className={'w-full bg-red-roulette text-white'}>
+									<motion.div
+										initial={{ x: '-300px' }}
+										animate={{ x: '100%' }}
+										transition={{ duration: 10, ease: 'linear', repeat: Number.POSITIVE_INFINITY }}
+										className={''}
+									>
+										This is a test version of BETFIN
+									</motion.div>
+								</div>
+							)}
 							<RootLayout id={id}>
 								<Outlet />
 							</RootLayout>
